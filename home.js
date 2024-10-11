b5c5589e9c9d13fe4cb1c5e5b337a341a3324289
Nzorov Emmanuel <116069622+Nzorovemmy@users.noreply.github.com>
2024-10-11 08:51:50 +0100

    Add files via upload

--- home.js
+++ home.js
@@ -1,3 +1,9 @@
 function ctaAction() {
     alert("Welcome to Ezydevs Studio! Let's build your dream website.");
 }
+
+function getStarted() {
+    alert('Redirecting to the signup page...');
+    // Here you can redirect to your signup or registration page
+    window.location.href = '/signup';
+}
